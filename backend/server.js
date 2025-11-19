3db9e40b0cb61dc080cb30018b7e8620e0011558
J-Gregorio <24302134@utfv.edu.mx>
2025-11-18 21:09:54 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const path = require("path");
 const Producto = require("./models/producto");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 
 app.use(cors());
